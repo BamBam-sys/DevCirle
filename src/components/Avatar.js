344116ff45adc77e99fda98765ed37c274959f9d
BamBam-sys <lawalayobamidavid@gmail.com>
2021-08-14 16:29:17 +0100

    mad chaanges

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,36 +1,32 @@
-
 //To be done
-
-
 
 // import React from 'react'
 // import { useState, useEffect} from 'react';
 
 // export const Avatar = (props) => {
 //     const [avatar, setAvatar] = useState([]);
-//     const {id} = props 
-
+//     const {id} = props
 
 //     useEffect(() => {
-//         let response = 
-        
+//         let response =
+
 //     }, [input])
 //     return (
 //         <div>
-            
+
 //         </div>
 //     )
 // }
 
-import React from 'react'
-import style from '../styles/Avatar.module.css'
+// import React from 'react'
+// import style from '../styles/Avatar.module.css'
 
-function Avatar(props) {
-  return (
-  <div>
-    <img className={style.circle-img} src={props.img} alt='chally' />
-  </div>
-  )
-}
+// function Avatar(props) {
+//   return (
+//   <div>
+//     <img className={style.circle-img} src={props.img} alt='chally' />
+//   </div>
+//   )
+// }
 
-export default Avatar
+// export default Avatar
