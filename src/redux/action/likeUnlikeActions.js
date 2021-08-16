0de8aff7836602ef01fad5209294aaa036156f38
BamBam-sys <lawalayobamidavid@gmail.com>
2021-08-16 18:25:38 +0100

    changes

--- src/redux/action/likeUnlikeActions.js
+++ src/redux/action/likeUnlikeActions.js
@@ -6,3 +6,23 @@ const { BASEURL } = config;
 
 export const liked = () => ({ type: LIKED });
 export const unliked = () => ({ type: UNLIKED });
+
+export const likeAsync = (data) => async (dispatch) => {
+  try {
+    const response = await axios.post(`/users/${loggedInUserId}`, toUserId);
+
+    dispatch(liked());
+
+    // setUserSession(response.data.data.token.split(" ")[1], response.data.data);
+  } catch (err) {}
+};
+
+export const UnlikeAsync = (data) => async (dispatch) => {
+  try {
+    const response = await axios.post(`/users/${loggedInUserId}`, toUserId);
+
+    dispatch(liked());
+
+    // setUserSession(response.data.data.token.split(" ")[1], response.data.data);
+  } catch (err) {}
+};
